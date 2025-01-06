688c4b89c53bb8d070e5bd89edb7b4b3c7e25f21
avocadojesus <jgarbey@gmail.com>
2025-01-06 09:25:30 -0800

    add fspec start/stop commands
    related https://rvohealth.atlassian.net/browse/PDTC-6651

--- src/index.ts
+++ src/index.ts
@@ -14,4 +14,21 @@ export async function createPsychicServer(PsychicServer: any) {
   return server
 }
 
+let _fspecServer: any = undefined
+export async function startFspecServer(PsychicServer: any, { port }: { port?: number } = {}) {
+  if (_fspecServer) return _fspecServer
+
+  const server = new PsychicServer()
+  await server.start(port || parseInt(process.env.DEV_SERVER_PORT || '7778'))
+
+  _fspecServer = server
+
+  return server
+}
+
+export async function stopFspecServer() {
+  await _fspecServer?.stop({ bypassClosingDbConnections: true })
+  _fspecServer = undefined
+}
+
 export const specRequest = _specRequest
