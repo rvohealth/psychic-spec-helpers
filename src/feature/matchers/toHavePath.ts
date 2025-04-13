5b55f9b849cbbb8fe0b9df1ece9e77a3dc1c1367
avocadojesus <jgarbey@gmail.com>
2025-04-13 12:54:51 -0700

    fix assertions

--- src/feature/matchers/toHavePath.ts
+++ src/feature/matchers/toHavePath.ts
@@ -1,25 +1,13 @@
 import { Page } from 'puppeteer'
-import evaluateWithRetryAndTimeout from '../internal/evaluateWithRetryAndTimeout.js'
 import requirePuppeteerPage from '../internal/requirePuppeteerPage.js'
 
 export default async function toHavePath(page: Page, expectedPath: string) {
   requirePuppeteerPage(page)
 
-  return await evaluateWithRetryAndTimeout(
-    page,
-    async () => {
-      const pathname = new URL(page.url()).pathname
-
-      return {
-        pass: pathname === expectedPath,
-        actual: expectedPath,
-      }
-    },
-    {
-      successText: () => {
-        throw new Error('cannot negate toHavePath')
-      },
-      failureText: () => `Expected page not to have path: "${expectedPath}"`,
-    }
+  await page.waitForFunction(
+    // @ts-expect-error window
+    path => new URL(window.location.href).pathname === expectedPath,
+    {},
+    expectedPath
   )
 }
